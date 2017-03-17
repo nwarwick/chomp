7094158a08cd11e51b4ed392ab76a41854830b71
William Sease <williamtsease@gmail.com>
2017-03-16 18:49:25 -0700

    Extracted controls to separate code, so they can be activated by either player or AI -- disabled boost until I/we can figure out how it ought to work (timing/speed/duration/stamina/etc)

--- ChompWorm/Assets/headMovement.cs
+++ ChompWorm/Assets/headMovement.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class headMovement : MonoBehaviour {
+public class headMovement : MonoBehaviour 
+{
 	// BASIC MOVEMENT VALUES
-	public float topSpeed;		// maximum movement speed (if the player's mouse is relatively far away)
+	public float topSpeed;		// maximum movement speed (when not boosting)
 	// BOOST VALUES
 	public float coolMax;	// this is the "full" value of the boost cooldown
 	public float coolReg;	// recovery rate for cooldown
-	//public float 
+	// INTERNAL MOVEMENT VARIABLES
+	public float moveSpeed = 0;  // (this is only "public" so playerControls and AI can get at it)
 	// INTERNAL BOOST VARIABLES
 	float coolCurrent = 0;
 	float speedBoost = 1;
@@ -17,14 +19,11 @@ public class headMovement : MonoBehaviour {
 	
 	void FixedUpdate () 
 	{
-		// rotation code is taken from Homework 2, there might be an easier way to do this?
-		var relativeMP = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-		float mouseDistance2 = relativeMP.y*relativeMP.y + relativeMP.x * relativeMP.x;	// (distance squared)
+		if (moveSpeed > topSpeed)
+			moveSpeed = topSpeed;
+		gameObject.transform.position += gameObject.transform.up * moveSpeed * Time.deltaTime;
 		
-		var relativeMA = Mathf.Atan2(relativeMP.y, relativeMP.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0, 0, relativeMA-90);
-		
-		var moveSpeed = speedBoost * topSpeed; // if the worm is currently boosting, the speed is maxed and multiplied
+/*		var moveSpeed = speedBoost * topSpeed; // if the worm is currently boosting, the speed is maxed and multiplied
 		if (speedBoost <= 1)
 		{
 			moveSpeed = (mouseDistance2)/(125f*125f);	// otherwise it is derived from mouse distance
@@ -32,23 +31,26 @@ public class headMovement : MonoBehaviour {
 				moveSpeed = topSpeed;					// and capped to the maximum speed
 		}
 		gameObject.transform.position += gameObject.transform.up * moveSpeed * Time.deltaTime;
-		
+		*/
 		if (coolCurrent > 0)
 			coolCurrent -= coolReg*Time.deltaTime;
 		if (speedBoost > 1)
 			speedBoost -= 2*Time.deltaTime;
 		
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			boost();
-		}
+		
 	}
 	
-	void boost()
+	public void turnTowards(Vector3 point)
+	{
+		var relativeMA = Mathf.Atan2(point.y, point.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, relativeMA-90);
+	}
+	
+	public void boost()
 	{
 		if (coolCurrent <= 0)
 		{
-			speedBoost = 6;
+			//speedBoost = 6; // HOW DOES BOOST ACTUALLY WORK
 			coolCurrent = coolMax;
 		}
 	}
---
+++ ChompWorm/Assets/playerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerControls : MonoBehaviour 
+{
+	headMovement headMovement;
+	void Start() 
+	{
+		headMovement = GetComponent<headMovement> ();
+	}
+	
+	void FixedUpdate () 
+	{
+		var mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		float mouseDistance = mousePosition.y*mousePosition.y + mousePosition.x * mousePosition.x;	// (actually distance squared)
+		
+		headMovement.turnTowards(mousePosition);
+
+		headMovement.moveSpeed = (mouseDistance)/((2000f)*(headMovement.topSpeed*headMovement.topSpeed));	// 2000f was chosen so that the worm will hit maximum speed at about 4 grid-squares away
+		
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			headMovement.boost();
+		}
+	}
+}
