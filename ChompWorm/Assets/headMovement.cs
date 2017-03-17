bcd070e0905a6d8e1716c8dde97b512233c33877
William Sease <williamtsease@gmail.com>
2017-03-16 17:29:46 -0700

    Added boost

--- ChompWorm/Assets/headMovement.cs
+++ ChompWorm/Assets/headMovement.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class headMovement : MonoBehaviour {
-	public float topSpeed;
-
-
+	// BASIC MOVEMENT VALUES
+	public float topSpeed;		// maximum movement speed (if the player's mouse is relatively far away)
+	// BOOST VALUES
+	public float coolMax;	// this is the "full" value of the boost cooldown
+	public float coolReg;	// recovery rate for cooldown
+	//public float 
+	// INTERNAL BOOST VARIABLES
+	float coolCurrent = 0;
+	float speedBoost = 1;
+	
 	void Start () {	}
 	
-	void FixedUpdate () {
+	void FixedUpdate () 
+	{
 		// rotation code is taken from Homework 2, there might be an easier way to do this?
 		var relativeMP = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
 		float mouseDistance2 = relativeMP.y*relativeMP.y + relativeMP.x * relativeMP.x;	// (distance squared)
@@ -16,9 +24,33 @@ public class headMovement : MonoBehaviour {
 		var relativeMA = Mathf.Atan2(relativeMP.y, relativeMP.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(0, 0, relativeMA-90);
 		
-		var moveSpeed = (mouseDistance2)/(125f*125f);	// this is so the worm moves slower/faster if the mous is closer/further
-		if (moveSpeed > topSpeed)
-			moveSpeed = topSpeed;
+		var moveSpeed = speedBoost * topSpeed; // if the worm is currently boosting, the speed is maxed and multiplied
+		if (speedBoost <= 1)
+		{
+			moveSpeed = (mouseDistance2)/(125f*125f);	// otherwise it is derived from mouse distance
+			if (moveSpeed > topSpeed)
+				moveSpeed = topSpeed;					// and capped to the maximum speed
+		}
 		gameObject.transform.position += gameObject.transform.up * moveSpeed * Time.deltaTime;
+		
+		if (coolCurrent > 0)
+			coolCurrent -= coolReg*Time.deltaTime;
+		if (speedBoost > 1)
+			speedBoost -= 2*Time.deltaTime;
+		
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			boost();
+		}
 	}
+	
+	void boost()
+	{
+		if (coolCurrent <= 0)
+		{
+			speedBoost = 6;
+			coolCurrent = coolMax;
+		}
+	}
+	
 }
--- ChompWorm/Assets/tailMovement.cs
+++ ChompWorm/Assets/tailMovement.cs
@@ -6,7 +6,7 @@ public class tailMovement : MonoBehaviour {
 
 	public GameObject leader;
 	public float stickDistance;
-	public float leaderDistance2;
+	float leaderDistance2;
 
 	void Start () {	}
 	
@@ -18,10 +18,8 @@ public class tailMovement : MonoBehaviour {
 		var leaderangle = Mathf.Atan2(leaderspot.y, leaderspot.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(0, 0, leaderangle-90);
 		
-		if (leaderDistance2 > stickDistance*stickDistance)
-		{
-			gameObject.transform.position += gameObject.transform.up * 3 * Time.deltaTime;
-		}
+		var moveSpeed = (leaderDistance2*2)/(stickDistance);	// as the leading object gets further away, the tail accellerates - this gives a "stretchy" motion
+		gameObject.transform.position += gameObject.transform.up * moveSpeed * Time.deltaTime;
 		
 	}
 }
