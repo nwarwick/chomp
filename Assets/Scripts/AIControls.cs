603863529ea6b92f9c7712d49c2651dbd3b3f3f5
William Sease <williamtsease@gmail.com>
2017-03-29 11:10:06 -0700

    Added basic grazing AI (eats food, ignores worms)

--- Assets/Scripts/AIControls.cs
+++ Assets/Scripts/AIControls.cs
@@ -4,22 +4,75 @@ using UnityEngine;
 
 public class AIControls : MonoBehaviour {
 	
-	// a super-simple AI for collision testing
-
 	public Head head;
-
+	public int state; // 0 Testing; 1 Wandering; 2 Grazing
+	
+	public GameObject target;	// the object the ai is currently moving towards, if any
+	Vector3 destination; // the location the ai is moving towards, when it doesn't have an object target
+	
+	// adjust these two floats if the size of the map changes
+	// (maybe they can somehow be fetched from the game management object during Start() or something, to allow for variable board sizes?)
+	public float xBound = 80f;
+	public float yBound = 80f;
+	float wanderTimer;
+	
 	void Start() 
 	{
 		head = GetComponent<Head> ();
+		state = 1;
+		SetRandomDestination();
+	}
+	
+	void SetRandomDestination()
+	{
+		destination = new Vector3(Random.Range(-xBound, xBound), Random.Range(-yBound, yBound), 0f);
+		wanderTimer = Random.Range(5.0f, 30.0f);
 	}
 	
 	void FixedUpdate () 
 	{
-		Vector3 testPoint = new Vector3(50f, 0f, 0f);
+		// the AI always moves at maxiumum speed, since it has perfect reflexes and doesn't care about precision anyway
+		
+		if (state == 0)
+		{	// TESTING (moves straight right, to make collisions easy)
+			Vector3 testPoint = new Vector3(50f, 0f, 0f);
+			head.turnTowards(testPoint);
+			head.moveSpeed = head.topSpeed;
+		}
+		else if (state == 1)
+		{	// WANDERING (looking for food)
+			
+			var tempdestination = destination - transform.position;
+			if (wanderTimer <= 0.0f)
+				SetRandomDestination();
+			else if ((tempdestination.x * tempdestination.x) + (tempdestination.y + tempdestination.y) < 3)
+				SetRandomDestination();
+			
+			head.turnTowards(tempdestination);
+			wanderTimer -= Time.deltaTime;
+			
+			head.moveSpeed = head.topSpeed;
+		}
+		else if (state == 2)
+		{	// GRAZING (moving to the nearest food)
+			if (target == null)
+			{
+				state = 1;
+				SetRandomDestination();
+			}
+			
+			destination = target.transform.position - head.transform.position;
+			head.turnTowards(destination);
+			
+			head.moveSpeed = head.topSpeed;
+		}
+		
+		
+		/*Vector3 testPoint = new Vector3(50f, 0f, 0f);
 		head.turnTowards(testPoint);
 
 		head.moveSpeed = head.topSpeed;
 		
-		// at the moment, te AI doesn't boost
+		// at the moment, te AI doesn't boost*/
 	}
 }
---
+++ Assets/Scripts/AISpot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpot : MonoBehaviour {
+
+	public AIControls AIControls;
+	
+	void Start () 
+	{	
+		AIControls = transform.parent.gameObject.GetComponent<AIControls>();
+	}
+	
+	void Update () {}
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+        if (AIControls.state == 1 && other.gameObject.tag == "Food")
+		{	// the searching worm has found food to eat
+			AIControls.target = other.gameObject;
+			AIControls.state = 2;
+		}
+/*		if (AIControls.state == 2 && other.gameObject.tag == "Food")
+		{	// if it's already eating, it'll eat this new food after
+			// AIControls.foodqueue.Enqueue(other.gameObject); // THIS DOESN"T WORK FOR SOME REASON
+		}*/
+    }
+}
