9fdf4b61d97803d76d6b3c4e6e7289d1121653d3
Nick Warwick <n_warwick@yahoo.ca>
2017-03-28 18:27:03 -0700

    Use list of bodyparts instead

--- Assets/Scripts/WormDie.cs
+++ Assets/Scripts/WormDie.cs
@@ -2,39 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WormDie : MonoBehaviour {
+public class WormDie : MonoBehaviour
+{
 
-	public GameObject head;
-	public GameObject neck;
-	public GameObject tail0;
-	public GameObject tail1;
-	public GameObject tail2;
-	public GameObject tail3;
-	
-	void Start () {	}
-	
-	void Update () { }
-	
-	public void Kill()
-	{
-		head.GetComponent<HeadMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		head.GetComponent<HeadMovement>().dead = true;
+    public GameObject head;
+    /*public GameObject neck;
+    public GameObject tail0;
+    public GameObject tail1;
+    public GameObject tail2;
+    public GameObject tail3;*/
 
-		neck.GetComponent<TailMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		neck.GetComponent<TailMovement>().dead = true;
+    public Player player;
 
-		tail0.GetComponent<TailMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		tail0.GetComponent<TailMovement>().dead = true;
-		
-		tail1.GetComponent<TailMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		tail1.GetComponent<TailMovement>().dead = true;
-		
-		tail2.GetComponent<TailMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		tail2.GetComponent<TailMovement>().dead = true;
-		
-		tail3.GetComponent<TailMovement>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
-		tail3.GetComponent<TailMovement>().dead = true;
-		
-		Destroy (gameObject, 1f);
-	}
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    void Update() { }
+
+    public void Kill()
+    {
+
+
+        head.GetComponent<Head>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        head.GetComponent<Head>().dead = true;
+
+        for (int i = 0; i < player.bodyParts.Length; i++)
+        {
+            player.bodyParts[i].deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+            player.bodyParts[i].dead = true;
+        }
+
+        /* 
+        neck.GetComponent<Tail>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        neck.GetComponent<Tail>().dead = true;
+
+        tail0.GetComponent<Tail>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        tail0.GetComponent<Tail>().dead = true;
+
+        tail1.GetComponent<Tail>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        tail1.GetComponent<Tail>().dead = true;
+
+        tail2.GetComponent<Tail>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        tail2.GetComponent<Tail>().dead = true;
+
+        tail3.GetComponent<Tail>().deadAngle = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0f);
+        tail3.GetComponent<Tail>().dead = true;*/
+
+        Destroy(gameObject, 1f);
+    }
 }
