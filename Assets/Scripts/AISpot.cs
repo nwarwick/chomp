52360cd25c506683fd6065d503be429b36271b58
Nick Warwick <n_warwick@yahoo.ca>
2017-04-06 20:52:51 -0700

    Comment out unused code

--- Assets/Scripts/AISpot.cs
+++ Assets/Scripts/AISpot.cs
@@ -19,18 +19,18 @@ public class AISpot : MonoBehaviour {
 		{	// if it sees the other worm's head first, it assumes it's being hunted
 			AIControls.state = 3;
 			AIControls.target = other.gameObject;
-			AIControls.foodQueue1 = null;
+			/*AIControls.foodQueue1 = null;
 			AIControls.foodQueue2 = null;
-			AIControls.foodQueue3 = null;
+			AIControls.foodQueue3 = null;*/
 			
 		}
 		else if (other.gameObject.tag == "Body" && other.gameObject.transform.parent != gameObject.transform.parent.parent && AIControls.state != 3)
 		{	// otherwise if it sees the tail first, it sees an opportunity (not its OWN tail, obviously)
 			AIControls.state = 4;
 			AIControls.target = other.gameObject;
-			AIControls.foodQueue1 = null;
+			/*AIControls.foodQueue1 = null;
 			AIControls.foodQueue2 = null;
-			AIControls.foodQueue3 = null;
+			AIControls.foodQueue3 = null;*/
 		}
         else if (AIControls.state == 1 && other.gameObject.tag == "Food")
 		{	// the searching worm has found food to eat
@@ -40,12 +40,12 @@ public class AISpot : MonoBehaviour {
 		else if (AIControls.state == 2 && other.gameObject.tag == "Food")
 		{	// if it's already eating, it'll queue this food to eat after
 
-			if (AIControls.foodQueue1 == null)
+			/*if (AIControls.foodQueue1 == null)
 				AIControls.foodQueue1 = other.gameObject;
 			else if (AIControls.foodQueue2 == null)
 				AIControls.foodQueue2 = other.gameObject;
 			else
-				AIControls.foodQueue3 = other.gameObject;
+				AIControls.foodQueue3 = other.gameObject;*/
 			
 			//AIControls.foodQueue.Enqueue(other.gameObject); // this is what we'd LIKE to be doing, intead of a hard-coded set of objects pretending to be a queue
 		}
