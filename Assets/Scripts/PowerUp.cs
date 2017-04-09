d76d8319c0f7cb29fa972422925e4f5c846579ea
Nick Warwick <n_warwick@yahoo.ca>
2017-04-09 11:09:22 -0700

    Add first powerup

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("POWERING UP");
+		if(gameObject.tag == "UltimatePowerUp")
+		{
+			EnableUltimate(other.gameObject);
+		}
+	}
+
+
+	void EnableUltimate(GameObject receiver)
+	{
+		// TODO
+	}
+}
