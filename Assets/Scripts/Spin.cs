3deb118cd8f96c054e9e6171f441d53c7927630a
Nick Warwick <n_warwick@yahoo.ca>
2017-04-09 11:05:14 -0700

    Add item spin animation

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float xSpeed = 10f;
+	public float ySpeed = 10f;
+	public float zSpeed = 10f;
+    
+    void Update ()
+    {
+		transform.Rotate(Vector3.right, zSpeed * Time.deltaTime);
+		transform.Rotate(Vector3.up, ySpeed * Time.deltaTime);
+		transform.Rotate(Vector3.forward, zSpeed * Time.deltaTime);
+    }
+}
