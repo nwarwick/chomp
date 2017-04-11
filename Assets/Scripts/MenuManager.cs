e89ece696a144b75de8e0415857fbc8e134d3a31
Nick Warwick <n_warwick@yahoo.ca>
2017-04-10 17:32:15 -0700

    Add menu manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    // Start the game
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Open options menu
+    public void Options()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    // Open about menu
+    public void About()
+    {
+        SceneManager.LoadScene("AboutMenu");
+    }
+
+    // Open highscore menu
+    public void HighScore()
+    {
+        SceneManager.LoadScene("HighScoreMenu");
+    }
+
+    // Open main menu
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Restart the game
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Quit the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
