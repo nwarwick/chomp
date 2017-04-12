bd236d0e99d4f6e1564d3cac79cd0d28f996413d
William Sease <williamtsease@gmail.com>
2017-04-11 23:56:14 -0700

    Composed music loop, placed it on main camera (where audio listener is) for lack of anywhere better - can be muted by pressing 'm', muted status carries between scenes

---
+++ Assets/Scripts/MuteMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteMusic : MonoBehaviour {
+
+	public AudioSource musicplayer;
+	
+	void Update () {
+		if (Input.GetKeyDown("m"))
+		{
+			if (musicplayer.volume > 0.5f)
+			{
+				MasterVolume.musicVolume = 0f;
+			}
+			else
+			{
+				MasterVolume.musicVolume = 1f;
+			}
+			
+			
+		}
+		musicplayer.volume = MasterVolume.musicVolume;
+	}
+}
