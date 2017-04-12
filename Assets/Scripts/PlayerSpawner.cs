aad1134a76f87b612ba76533a6e9c6e5d533a164
William Sease <williamtsease@gmail.com>
2017-04-11 23:05:05 -0700

    Fixed player boost-spread bug by spawning in a player as the game begins (also, the player gets a random colour

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public GameObject playerWormPrefab; 
+	public CameraFollow CameraFollow;
+	
+	void Start () {
+		Vector3 boardMiddle = new Vector3(0f, 0f, 0f);
+		GameObject newWorm = Instantiate(playerWormPrefab, boardMiddle, Quaternion.identity);
+		//Create random color
+   		Color randColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+		SpriteRenderer[] bodySprites =  newWorm.GetComponentsInChildren<SpriteRenderer>();
+		// Make the worm a random colour because it is fun
+		for(int i = 0; i < bodySprites.Length; i ++)
+		{
+			bodySprites[i].material.color = randColor;
+		}
+		
+		CameraFollow.player = newWorm.transform.GetChild(0).gameObject;	// yes yes I know it shouldn't be referenced by index but it's 11:30 at night
+	}
+	
+	
+}
